9caa72ffdb2837857b51baa2ed4ef2bc98f8da1a
Diego Alberto Nieto Flores <DNIETO@invercap.icap>
2020-03-25 12:21:01 -0600

    Add project files.
    prueba

---
+++ Api_GestionFC/Models/ResultadoEjecucion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_GestionFC.Models
+{
+    public class ResultadoEjecucion
+    {
+        public bool EjecucionCorrecta { get; set; }
+        public string ErrorMessage{ get; set; }
+        public string FriendlyMessage { get; set; }
+
+    }
+}
