aa35f21ade25b27b7ad881cacc776e44dd785ef7
Camilo <cammilo.grimmaldo@hotmail.com>
2020-03-25 20:49:41 -0600

    Log Sistema
    para ejecutar el GFC.Spi_LogSistema

---
+++ Api_GestionFC/DTO/LogSistemaDTO.cs
@@ -0,0 +1,16 @@
+﻿using Api_GestionFC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_GestionFC.DTO
+{
+    public class LogSistemaDTO
+    {
+        public LogSistema LogSistema { get; set; }
+        public ResultadoEjecucion ResultadoEjecucion { get; set; }
+
+
+    }
+}
---
+++ Api_GestionFC/Models/LogSistema.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_GestionFC.Models
+{
+    public class LogSistema
+    {
+        [Key]
+        public int IdAccion { get; set; }
+        public int IdPantalla { get; set; }
+        public int Usuario { get; set; }
+        public string Dispositivo { get; set; }
+    }
+}
