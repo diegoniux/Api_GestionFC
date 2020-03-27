1cbfba281882a60bc31df1f3c6dfe0391094d9b9
Richi Garza <richi.garzar@gmail.com>
2020-03-26 19:32:12 -0600

    Update LoginRepository.cs
    Se realiza funcionalidad basica de login, pendiente acomodar clases y función generica donde corresponde.

--- Api_GestionFC/Repository/LoginRepository.cs
+++ Api_GestionFC/Repository/LoginRepository.cs
@@ -7,7 +7,9 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,56 +27,90 @@ namespace Api_GestionFC.Repository
             this._configuration = configuration;
         }
 
-        public LoginDTO LoginUser(LoginData loginData)
+        public string EnvioPeticionRest(string json)
         {
+            string Resultado = string.Empty;
             try
             {
-                //Cídigo para hacer el lógin del usuario
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://desaiis01/ICAP_AfiliacionServices_GFC/GerentesFC/GerentesFCService.svc/ObtieneDatosUsuario");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                httpWebRequest.Timeout = 600000;
 
-
-
-
-                var usuario = new Usuario()
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                 {
-                    Nomina = 23401,
-                    NombreCompleto = "Dieguito Maradona",
-                    Email = "dnieto@invercap.com.mx"
-                };
+                    streamWriter.Write(json);
+                }
 
-
-                // authentication successful so generate jwt token
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var key = Encoding.ASCII.GetBytes(_configuration["Secret"]);
-                var tokenDescriptor = new SecurityTokenDescriptor
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
                 {
-                    Subject = new ClaimsIdentity(new Claim[]
-                    {
-                    new Claim("userData", JsonConvert.SerializeObject(usuario) )
-                    }),
-                    Expires = DateTime.UtcNow.AddMinutes(1),
-                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-                };
-                var token = tokenHandler.CreateToken(tokenDescriptor);
-                string Token = tokenHandler.WriteToken(token);
-
-                return new LoginDTO()
-                {
-                    Usuario = usuario,
-                    ResultadoEjecucion = new ResultadoEjecucion() { EjecucionCorrecta = true },
-                    Token = Token
-                };
+                    var result = streamReader.ReadToEnd();
+                    Resultado = result.ToString();
+                }
             }
             catch (Exception ex)
             {
-                return new LoginDTO()
-                {
-                    Usuario = new Usuario(),
-                    ResultadoEjecucion = new ResultadoEjecucion() { EjecucionCorrecta = false, ErrorMessage = ex.Message, FriendlyMessage = "Ocurrió un error" },
-                    Token = null
-                };
+                throw ex;
             }
+            return Resultado;
         }
 
+        public LoginDTO LoginUser(LoginData loginData)
+        {
+            LoginDTO Response = new LoginDTO();
+            try
+            {
+                //Código para hacer el lógin del usuario
+                string json = "{ \"nomina\": " + loginData.Nomina.ToString() + ", \"password\": \"" + loginData.Password + "\" }";
 
+                ObtieneDatosUsuarioJsonResponse jsonResult = JsonConvert.DeserializeObject<ObtieneDatosUsuarioJsonResponse>(EnvioPeticionRest(json));
+
+                if (jsonResult.ObtieneDatosUsuarioResult.Autorizado)
+                {
+                    Response.Usuario = new Usuario()
+                    {
+                        Nomina = loginData.Nomina,
+                        NombreCompleto = jsonResult.ObtieneDatosUsuarioResult.NombreCompleto,
+                        Email = string.Empty
+                    };
+
+
+                    // authentication successful so generate jwt token
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var key = Encoding.ASCII.GetBytes(_configuration["Secret"]);
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new ClaimsIdentity(new Claim[]
+                        {
+                            new Claim("userData", JsonConvert.SerializeObject(Response.Usuario) )
+                        }),
+                        Expires = DateTime.UtcNow.AddMinutes(1),
+                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                    };
+                    var token = tokenHandler.CreateToken(tokenDescriptor);
+                    Response.Token = tokenHandler.WriteToken(token);
+                    Response.ResultadoEjecucion = new ResultadoEjecucion() { EjecucionCorrecta = true, ErrorMessage = null, FriendlyMessage = null };
+                }
+                else throw new Exception("Usuario no autorizado.");
+            }
+            catch (Exception ex)
+            {
+                Response.Usuario = null;
+                Response.ResultadoEjecucion = new ResultadoEjecucion() { EjecucionCorrecta = false, ErrorMessage = ex.Message, FriendlyMessage = "Ocurrió un error" };
+                Response.Token = null;
+            }
+            return Response;
+        }
+
+        public class ObtieneDatosUsuarioResponse
+        {
+            public string NombreCompleto { get; set; }
+            public bool Autorizado { get; set; }
+        }
+        public class ObtieneDatosUsuarioJsonResponse
+        {
+            public ObtieneDatosUsuarioResponse ObtieneDatosUsuarioResult { get; set; }
+        }
     }
 }
