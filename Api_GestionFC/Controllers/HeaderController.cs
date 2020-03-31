7ab78dccaf193152095aa2e639fb2a49ebcd20d1
Richi Garza <richi.garzar@gmail.com>
2020-03-31 14:38:51 -0600

    header

--- Api_GestionFC/Controllers/HeaderController.cs
+++ Api_GestionFC/Controllers/HeaderController.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using DTO = Api_GestionFC.DTO;
+using Repository = Api_GestionFC.Repository;
 
 namespace Api_GestionFC.Controllers
 {
-    public class HeaderController
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class HeaderController : ControllerBase
     {
+        private readonly Repository.HeaderRepository _repository;
+        public HeaderController(Repository.HeaderRepository repository) 
+        {
+            this._repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("GetHeader/{nomina}")]
+        public async Task<DTO.GridPromotoresDTO> GetHeader(int nomina)
+        {
+            return await _repository.GetHeader(nomina);
+        }
     }
 }
--- Api_GestionFC/Repository/HeaderRepository.cs
+++ Api_GestionFC/Repository/HeaderRepository.cs
@@ -1,11 +1,66 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using DTO = Api_GestionFC.DTO;
 
 namespace Api_GestionFC.Repository
 {
     public class HeaderRepository
     {
+        private readonly string _connectionString;
+        private readonly IConfiguration _configuration;
+
+        public HeaderRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("AfiliacionDB");
+            this._configuration = configuration;
+        }
+
+        public async Task<DTO.HeaderDTO> GetHeader(int nomina)
+        {
+            var response = new DTO.GridPromotoresDTO();
+            try
+            {
+                using (SqlConnection sqlConn = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand sqlCmd = new SqlCommand("GFC.Spi_LogSistema", sqlConn))
+                    {
+                        sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                        sqlCmd.Parameters.AddWithValue("@p_Nomina", nomina);
+
+                        await sqlConn.OpenAsync();
+
+                        using (var reader = await sqlCmd.ExecuteReaderAsync())
+                        {
+                            await reader.ReadAsync();
+                            response.Promotores.Add();
+
+                            response.ResultadoEjecucion.EjecucionCorrecta = (bool)reader["EjecucionCorrecta"];
+                            //Si la ejecución es exitosa
+                            if (response.ResultadoEjecucion.EjecucionCorrecta)
+                            {
+                                response.ResultadoEjecucion.ErrorMessage = reader["Mensaje"].ToString();
+                                response.ResultadoEjecucion.FriendlyMessage = reader["Mensaje"].ToString();
+                            }
+                            else
+                            {
+                                response.ResultadoEjecucion.ErrorMessage = reader["Mensaje"].ToString();
+                                response.ResultadoEjecucion.FriendlyMessage = reader["Mensaje"].ToString();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return response;
+        }
     }
 }
